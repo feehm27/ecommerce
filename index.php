19d8b3fc3e73a473baf61625e5074de8d910a119
Fernanda <feehmfontoura@hotmail.com>
2019-01-28 21:11:22 -0200

    Inicio do Projeto

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("vendor/autoload.php");
 
 $app = new \Slim\Slim();
 
-$app->config('debug', true);
+$app->config('debug', true); // modo debug ligado
 
 $app->get('/', function() {
     
